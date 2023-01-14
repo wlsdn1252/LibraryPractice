68fa49a289dfae38de10664536c1a5bcb2df7688
강진우 <wlsdn1252@naver.com>
2023-01-15 00:44:15 +0900

    웹상의 이미지 불러오기(Glide 활용) 인터넷 권한 획득

--- app/src/main/java/com/example/librarypractice/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.librarypractice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this,ViewProfilePhotoActivity :: class.java)
             startActivity(myIntent)
         }
+
+        // 인터넷에 있는 이미지(https:)로 시작하는 이미지주소를 사용
+        // glide 라이브러리 활용
+        Glide.with(this).load("https://i0.wp.com/breakingcube.com/wp-content/uploads/2020/10/%EB%AC%B4%EB%A3%8C-" +
+                "%EC%9D%B4%EB%AF%B8%EC%A7%80-%EB%B8%94%EB%A1%9C%EA%B7%B8-%ED%8F%AC%EC%8A%A4%ED%8C%85-%EC%8D%B8%EB%84%A4%EC%9D%BC-1." +
+                "png?resize=1024%2C576&ssl=1").into(lectureImg1)
 
     }
 }
