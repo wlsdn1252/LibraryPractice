861b5f213aabc305c9bf34971e50d116de1ab83b
강진우 <wlsdn1252@naver.com>
2023-01-15 00:09:01 +0900

    프로필 크게 보는 화면 추가 및 인텐트 작성

--- app/src/main/java/com/example/librarypractice/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.librarypractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profileImg.setOnClickListener {
+            // 프로필 크게 보는 액티비티로 이동
+            val myIntent = Intent(this,ViewProfilePhotoActivity :: class.java)
+            startActivity(myIntent)
+        }
     }
 }
